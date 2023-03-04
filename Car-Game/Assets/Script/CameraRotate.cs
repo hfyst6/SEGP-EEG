8396e8dfd63cf50f745e8305fd5e08558988f362
hfyst6 <94058547+hfyst6@users.noreply.github.com>
2023-03-05 01:37:46 +0800

    Added minimap

---
+++ Car-Game/Assets/Script/CameraRotate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public GameObject CarCamera;
+    public GameObject RotateCamera;
+    public GameObject Timer;
+
+    public Transform AudioListener;
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.parent = RotateCamera.transform;
+        transform.Rotate(0,0.8f,0,Space.World);
+
+        if(Timer.activeSelf == true){
+            RotateCamera.SetActive(false);
+            CarCamera.SetActive(true);
+            AudioListener.parent = CarCamera.transform;
+        }
+    }
+}
