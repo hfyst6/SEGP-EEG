f65aab808de600329d9eaa654c20d42f69e440b3
Mohamed Abubaker <abubaker@vimigotech.com>
2023-03-14 15:49:33 +0800

    init

---
+++ running-game/Assets/Scripts/Collectables/CollectCoin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour {
+	public AudioSource coinFX;
+
+	void OnTriggerEnter(Collider other)
+	{
+		coinFX.Play();
+		this.gameObject.SetActive (false);
+	}
+}
