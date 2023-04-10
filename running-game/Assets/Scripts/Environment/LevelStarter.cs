451c9f8de029f54ad9c13764790533e03bdfeaa7
Mohamed Abubaker <M7.abubaker@gmail.com>
2023-04-11 07:25:06 +0800

    count down

---
+++ running-game/Assets/Scripts/Environment/LevelStarter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStarter : MonoBehaviour {
+	public GameObject countDown3;
+	public GameObject countDown2;
+	public GameObject countDown1;
+	public GameObject countDownG0;
+	public GameObject fadeIn;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (CountSequence ());
+		
+	}
+	IEnumerator CountSequence(){
+		yield return new WaitForSeconds(1.5f);
+		countDown3.SetActive (true);
+		yield return new WaitForSeconds(1);
+		countDown2.SetActive (true);
+		yield return new WaitForSeconds(1);
+		countDown1.SetActive (true);
+		yield return new WaitForSeconds(1);
+		countDownG0.SetActive (true);
+	}
+
+}
